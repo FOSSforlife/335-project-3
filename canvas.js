1c172f9e4136526a464b943ce21e445f05e6266c
AS Paul <aspaul2@gmail.com>
2019-05-04 09:56:06 -0700

    Update canvas.js
    Cleaned things up even more, added a function to draw a path

--- canvas.js
+++ canvas.js
@@ -25,9 +25,9 @@ for (var i=0; i<square_size*10; i+=square_size){
 
 }
 
-draw_grid();
 
-//to feed into the draw line and draw circle function
+//to feed into the draw line and draw circle function. Translates the basic grid coordinate into the dimensions of the grid
+//so if you wanted to see what cell (0, 1) was in terms of the grid you would do get_point(0) get_point(1)
 function get_point(cell){
 	return (square_size/2)+(square_size*cell);
 }
@@ -50,19 +50,11 @@ function draw_circle_at_coordinate(x,y){
 }
 
 
-//marks source point at 1,2 (indexed from zero)
-c.strokeStyle= 'blue';
-draw_circle_at_coordinate(1,2)
-
-
-//marks sink at 8,7 
-c.strokeStyle= 'red';
-draw_circle_at_coordinate(8,7);
 
 
 
 
-//create 2d array of the flow capacity (this function is already in the main file)
+//create 2d array of the flow capacity ***this function is already in the main file***
 function createGrid() {
   let gridRows = new Array(10);
   gridRows.fill(new Array(10).fill(0));
@@ -76,10 +68,10 @@ function createGrid() {
 }
 
 
-var flow_values= createGrid();
 
-//draws randomized flow values on the canvas, saved in the 2d array above (make sure it is the same values that the algorithm is using)
-function draw_flow_capacity(){
+
+//draws randomized flow values on the canvas, saved in the 2d array created by createGrid. (change font/textsize if desired here)
+function draw_flow_capacity(flow_values){
 	for (var i=0; i<10; i++){
 		x_coordinate= i * square_size+ square_size/3;
 		for (var j=0; j<10; j++){
@@ -93,42 +85,50 @@ function draw_flow_capacity(){
 }
 
 
-draw_flow_capacity();
-
-
-
-
 //this is going to help draw our path, connecting the circles
 function draw_line(x_start, y_start, x_end, y_end){
 	c.beginPath();
 	c.moveTo(get_point(x_start), get_point(y_start));
 	c.lineTo(get_point(x_end), get_point(y_end));
-	c.strokeStyle= "#fa34a3";
 	c.stroke();
 }
 
 
+//Given an array filled with subarrays, where each subarray is a cell, draw the path.
+function draw_knight_path(path_array){
+	for (var i=0; i<path_array.length-1; i++){
+		var start_cell= path_array[i];
+		var end_cell= path_array[i+1];
+		c.strokeStyle= 'grey';
+		c.lineWidth=1;
+		draw_circle_at_coordinate(start_cell[0], start_cell[1]);
+		draw_line(start_cell[0],start_cell[1], end_cell[0], end_cell[1]);
+	}
 
-//just testing the draw line function
-draw_line(0,0, 2,4);
+	}
+
+//-------Drawing------	
+
+//*******make sure this is the same values that the algorithm is using and not creating another instance **********
+var flow_values= createGrid();
+
+draw_grid();
+draw_flow_capacity(flow_values);
+
+//marks source point at 1,2 (indexed from zero)
+c.strokeStyle= 'blue';
+c.lineWidth=5;
+draw_circle_at_coordinate(1,2)
 
 
+//marks sink at 8,7 
+c.strokeStyle= 'red';
+draw_circle_at_coordinate(8,7);
 
-//creates an animation loop, may use this to show position of knight as it moves along its path
-// function animate(){
-// 	requestAnimationFrame(animate);
-// 	// c.beginPath();
-// 	// c.arc(x, 200, 15, 0, Math.PI* 2, false);
-// 	// c.strokeStyle= 'blue';
-// 	// c.stroke();
-// 	c.moveTo(50, 300);
-// 	c.lineTo(x, y);
-// 	c.strokeStyle= "#fa34a3";
-// 	c.stroke();
 
-// 	x+=1;
-// }
-// animate();
+//just testing the path.  
+var test_path= [[1,2], [2,4], [3,6], [4,5]];
+draw_knight_path(test_path);
 
 
 
